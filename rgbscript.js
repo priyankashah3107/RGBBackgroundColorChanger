2038e62cafa282da5efd9c52014abf04b6cfbf97
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-02-28 22:29:56 +0530

    Add files via upload
    RGB Background Background Color changer

---
+++ rgbscript.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+    document.querySelector('#red').addEventListener('click', function() {
+     body.style.backgroundColor = 'red';
+    });
+    document.querySelector('#green').addEventListener('click', function() {
+     body.style.backgroundColor = 'green';
+    });
+    document.querySelector('#blue').addEventListener('click', function() {
+     body.style.backgroundColor = 'blue';
+    });
